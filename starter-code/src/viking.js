0048a119715e3ccf5ceffb5aa0ab894dd8f0423b
Andrew Nunez <22259@columbushs.com>
2019-12-09 08:43:36 -0500

    almost done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,26 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+    recieveDamage(damage){
+        this.health -= null
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor (name, health, strength){
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+    }
+    
+}
 
 // Saxon
 class Saxon {}
