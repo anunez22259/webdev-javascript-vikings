5f048073ac1d9538e1f1ca77c6916fc0963eb116
Andrew Nunez <22259@columbushs.com>
2019-12-10 14:44:20 -0500

    almost almost done

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -8,22 +8,53 @@ class Soldier {
         return this.strength;
     }
     recieveDamage(damage){
-        this.health -= null
+        this.health=this.health-damage;
     }
 }
 
 // Viking
 class Viking extends Soldier{
     constructor (name, health, strength){
+        super(health, strength)
         this.name = name;
-        this.health = health;
-        this.strength = strength;
     }
-    
+    recieveDamage(damage){
+        this.health -= damage;
+        if (this.health > 0){
+            return `${this.name} has recieved ${damage} points of damage`;
+        } else if (this.health <= 0) {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+    battleCry(){
+        return "Odin Owns You All!";
+    }
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+
+    recieveDamage(damage){
+        super.recieveDamage(damage);
+        return (this.health > 0) ? `A Saxon has recieved ${damage} points of damage` : `A Saxon has died in combat`;
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.length = 0;
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+
+    }
+    addViking(Vikings){
+        this.vikingArray.push(Vikings)
+    }
+    addSaxon(Saxons){
+        this.saxonArray.push(Saxons)
+    }
+}
